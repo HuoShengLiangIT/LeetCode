2558a6d9fdc9f643df38778d56dcd9c747469808
jiaoyan <1483104508@qq.com>
2018-11-21 23:53:40 +0800

    I learned happily again

---
+++ _929/Solution.java
@@ -0,0 +1,22 @@
+package _929;
+
+import java.util.HashSet;
+
+public class Solution {
+    public int numUniqueEmails(String[] emails) {
+        HashSet<String> set = new HashSet<String>();
+        int len = emails.length;
+        for(int i=0;i<len;i++){
+            String x = emails[i];
+            String[] email = x.split("@");
+            StringBuilder b = new StringBuilder(email[0].split("\\+")[0]);
+            int del = 0;
+            while((del=b.indexOf("."))!=-1){
+                b.deleteCharAt(del);
+            }
+            b.append(email[1]);
+            set.add(b.toString());
+        }
+        return set.size();
+    }
+}

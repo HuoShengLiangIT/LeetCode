962fcd9558c6328925d33842ced7dbaf352ba8e7
jiaoyan <1483104508@qq.com>
2018-11-29 00:25:15 +0800

    I create 258 problem solution and add comment of English

---
+++ _258/Solution.java
@@ -0,0 +1,27 @@
+package _258;
+
+public class Solution {
+    /**
+     * simple solution
+     * @param num original num
+     * @return     result num
+     */
+    public int addDigits(int num) {
+        //i is the result
+        int i = num;
+        //if i>=10,continue to loop
+        while (i >= 10) {
+            //j is the num of middle result
+            int j = i;
+            //i init of 0
+            i = 0;
+            //loop of j,when it one bit is taken each time and then added to i
+            while (j != 0) {
+                i += j % 10;
+                j = j / 10;
+            }
+        }
+        //return result
+        return i;
+    }
+}

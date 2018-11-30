f640b8e36312d727a574df2b129bdd1855096e09
jiaoyan <1483104508@qq.com>
2018-11-30 18:19:51 +0800

    Solve the 520th question and add an English comment

---
+++ _520/Solution.java
@@ -0,0 +1,15 @@
+package _520;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public boolean detectCapitalUse(String word) {
+        int cnt=0;
+        //z is 90 in the ASCII code table
+        //this if can judge uppercase letters
+        for(char c : word.toCharArray()) if('Z'-c>=0) cnt++;
+        //cnt==0:all letters are lowercase letters;cnt==word.length() all letters are uppercase letters
+        //cnt==1&&'Z'-word.charAt(0)>=0:word have a uppercase letter and frist letter is uppercase
+        return((cnt==0 || cnt==word.length()) || (cnt==1 && 'Z'-word.charAt(0)>=0));
+    }
+}

1ef6bc1b76117af4e5907e7f237cdcc54e47baf1
LAPTOP-B2QGGJAQ\焦焱 <1483104508@qq.com>
2019-01-22 22:27:47 +0800

    22点27分

---
+++ _589/Solution.java
@@ -0,0 +1,35 @@
+package _589;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+
+public class Solution {
+    public List<Integer> preorder(Node root) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        pre(root,list);
+        return list;
+    }
+    public void pre(Node root,ArrayList<Integer> list){
+        if(root==null){
+            return;
+        }
+        list.add(root.val);
+        for(Node n:root.children){
+            pre(n,list);
+        }
+    }
+}
---
+++ _961/Solution.java
@@ -0,0 +1,21 @@
+package _961;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int repeatedNTimes(int[] A) {
+        Map<Integer, Integer> count = new HashMap();
+        for (int x: A) {
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+
+        for (int k: count.keySet()){
+            if (count.get(k) > 1){
+                return k;
+            }
+        }
+
+        throw null;
+    }
+}
